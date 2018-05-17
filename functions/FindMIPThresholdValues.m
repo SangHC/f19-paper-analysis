fbf9e817cd2b96297525f45edeb3b13db24b76df
Tyler Glass <tylerglass33@gmail.com>
2018-05-17 13:30:59 -0400

    change variable name to maxventilation

--- functions/FindMIPThresholdValues.m
+++ functions/FindMIPThresholdValues.m
@@ -1,4 +1,4 @@
-function [ low_vent, mid_vent, high_vent ] = FindMIPThresholdValues( MIP , f19_lung)
+function [ low_vent, mid_vent, high_vent, MaxVentilation ] = FindMIPThresholdValues( MIP , f19_lung)
 %Determines low, medium, high ventilation threshold using MIP
 % uses 1st and 18th slice of MIP which do not have f19 signal
 % MIP = entire MIP image
@@ -18,12 +18,12 @@ low_vent  = bgd_mean +  3*bgd_std; % 3 stds
 mid_vent  = bgd_mean +  6*bgd_std; % 6 stds
 high_vent = bgd_mean + 10*bgd_std; % 10 stds
 
-%% Compute Ventilation Thresholds using percent max ventilation
-insidelungMIIT = f19_lung(find(f19_lung>0));
-percentile90   = prctile(insidelungMIIT,90);
+%% Compute Ventilation Thresholds using percent of max ventilation
+insidelungMIITvals = f19_lung(find(f19_lung>0));
+MaxVentilation     = prctile(insidelungMIITvals,90);
 
-low_vent  = bgd_mean +  0.1*(percentile90-bgd_mean); % 10% of max ventilation
-mid_vent  = bgd_mean +  0.3*(percentile90-bgd_mean); % 30% of max ventilation
-high_vent = bgd_mean +  0.5*(percentile90-bgd_mean); % 50% of max ventilation
+low_vent  = bgd_mean +  0.1*(MaxVentilation-bgd_mean); % 10% of max ventilation
+mid_vent  = bgd_mean +  0.3*(MaxVentilation-bgd_mean); % 30% of max ventilation
+high_vent = bgd_mean +  0.5*(MaxVentilation-bgd_mean); % 50% of max ventilation
 
 end
